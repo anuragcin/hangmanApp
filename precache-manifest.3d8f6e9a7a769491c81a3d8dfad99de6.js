5b2e9414c491f71a8a7a1875d14b36367f539c80
Anurag <anurag@Tamannas-MacBook-Pro.local>
2019-12-19 18:33:47 -0800

    Updates

---
+++ precache-manifest.3d8f6e9a7a769491c81a3d8dfad99de6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a1799331d648975d8eb91505351bd30",
+    "url": "/hangmanApp/index.html"
+  },
+  {
+    "revision": "4f7a31a9fb883399e489",
+    "url": "/hangmanApp/static/css/main.68cf6e26.chunk.css"
+  },
+  {
+    "revision": "c2b68fe114e1ed728e52",
+    "url": "/hangmanApp/static/js/2.7dfc88bf.chunk.js"
+  },
+  {
+    "revision": "4f7a31a9fb883399e489",
+    "url": "/hangmanApp/static/js/main.9f7252e4.chunk.js"
+  },
+  {
+    "revision": "70ce72004c40403eeaa4",
+    "url": "/hangmanApp/static/js/runtime-main.f5a73aaa.js"
+  }
+]);
